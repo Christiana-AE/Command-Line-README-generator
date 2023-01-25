9aaf7c545564cf4e66866993a187955416a3a780
Christiana Anako <christianaanako@Christianas-MacBook-Air.local>
2023-01-25 22:13:30 +0000

    Update of question to populate the README file

--- index.js
+++ index.js
@@ -4,9 +4,67 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+const questions = () => 
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'What is your project title?',
+        },
 
-];
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Describe your project',
+        },
+
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'Describe project installation',
+        },
+
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'Describe how project is used',
+        },
+
+        {
+            type: 'list',
+            name: 'License',
+            message: 'What license is project using?',
+            choices: ['Apache License 2.0', 'MIT License', 'Eclipse Public license 2.0', 'None'],
+
+        },
+
+        {
+            type: 'input',
+            name: 'Contribution',
+            message: 'Enter contributors',
+        },
+
+        {
+            type: 'input',
+            name: 'Test',
+            message: 'Describe tests run',
+        },
+
+        {
+            type: 'input',
+            name: 'githubUser',
+            message: 'What is your github user name?',
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        }
+
+    ]
+
+    )
 
 // function to write README file
 function writeToFile(fileName, data) {
